8e1f2c4aabd90a21af8549f345a9bf4cbc7ab304
Ioana Surdu-Bob <ioana.sbob@gmail.com>
2018-03-01 16:43:54 +0000

    added search HTML wrapper

--- lib/drab_pagination_html.ex
+++ lib/drab_pagination_html.ex
@@ -3,6 +3,8 @@ defmodule DrabPagination.HTML do
   HTML helpers for sorting/searching
   """
   def sort_by_dropdown(entries_assigns_name, sorting_dropdown_options, opts \\ []) do
+    # computes HTML for sorting dropdown using the given parameters
+    # optional parameters will be empty string if not given
     "<select class=\"" 
     <> Keyword.get(opts, :class, "")
     <> "\" name=\"sort\" drab-change=\"sort_"
@@ -10,6 +12,22 @@ defmodule DrabPagination.HTML do
     <> "\">"
       |> add_options_to_dropdown(Map.to_list(sorting_dropdown_options))
       |> Phoenix.HTML.raw()
+  end
+
+  def search(entries_assigns_name, attribute, opts \\ []) do
+    # computes HTML for search input using the given parameters
+    # optional parameters will be empty string if not given
+    Phoenix.HTML.raw("<input name=\"search\" data-attr=\""
+                     <> Atom.to_string(attribute)
+                     <> "\" drab-keyup=\"search_"
+                     <> Atom.to_string(entries_assigns_name)
+                     <> "\" placeholder= \""
+                     <> Keyword.get(opts, :placeholder, "")
+                     <> "\" type=\"text\" id=\""
+                     <> Keyword.get(opts, :id, "")
+                     <> "\" class=\""
+                     <> Keyword.get(opts, :class, "")
+                     <> "\"> </input>")
   end
 
   defp add_options_to_dropdown(html, []), do: html <> "</select>"
